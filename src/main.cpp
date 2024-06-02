758430108926b03d5dd0f955938aff1ac66e34aa
Manish <manishsharma43005@proton.me>
2024-06-02 23:46:21 +0530

    A cross-platform logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include "SDL.h"
 #include "SDL_image.h"
+#include "think-lib.h"
 
 int main(int argc, char* argv[])
 {
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
-        return -1;
+        SM_ASSERT(false, SDL_GetError());
     }
 
     if (IMG_Init(IMG_INIT_PNG) == 0)
     {
-        return -1;
+        SM_ASSERT(false, IMG_GetError());
     }
-
-    // Your code here
 
     IMG_Quit();
     SDL_Quit();
